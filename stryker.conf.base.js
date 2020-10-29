a0d2992992aca0ebaff99aced102bb2246cc49e9
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-10-29 02:48:45 -0300

    chore(stryker): add stryker config

---
+++ stryker.conf.base.js
@@ -0,0 +1,8 @@
+module.exports = {
+  coverageAnalysis: "off",
+  mutator: "javascript",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress", "dashboard"],
+  testRunner: "jest",
+  transpilers: [],
+};
